0510a1860b3573626967751480a4d9dea15e500f
sprinkuls <maymacler@gmail.com>
2025-04-13 05:46:59 -0400

    some changes

---
+++ junk.js
@@ -0,0 +1,10 @@
+/* idea i'm putting here: do something with js to draw a background made of
+*  randomly positioned white circles on black bg, so it looks like stars :3
+*/
+
+console.log("hellow, orld!");
+
+let canvas = document.createElement("canvas");
+let context = canvas.getContext('2d');
+canvas.width = screen.width;
+canvas.height = screen.height;
